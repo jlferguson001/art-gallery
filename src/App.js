2fd754202908da7aaec095ac4426c55dce284513
Jenni Ferguson <98863330+jlferguson001@users.noreply.github.com>
2022-07-08 19:09:44 -0500

    added Gallery & Button Bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
+import Gallery from "./components/Gallery";
+import ButtonBar from "./components/ButtonBar";
 import './App.css';
 
 function App() {
+  /* State variables here... */
+  let [artId, setArtId] = useState(12720);
+  let [data, setData] = useState({});
+
+  useEffect(() => {
+    document.title = "Welcome to Artworld";
+    fetch(
+      `https://collectionapi.metmuseum.org/public/collection/v1/objects/${artId}`
+    )
+      .then((response) => response.json())
+      .then((resData) => setData(resData));
+  }, [artId]);
+
+  // send this function down to <ButtonBar />
+  const handleIterate = (e) => {
+    setArtId(artId + Number(e.target.value));
+  };
+
+  /* Return JSX down here... */
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Gallery
+        objectImg={data.primaryImage}
+        artist={data.artistDisplayName}
+        title={data.title}
+      />
+
+      <ButtonBar />
     </div>
   );
 }
